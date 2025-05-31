cf1bde1277f6d48a627bb666fccdfa4aa05c4270
Silmbaraselvan-15 <saransilambu007@gmail.com>
2025-05-31 22:35:40 +0530

    final processor

--- PacketProcessor.py
+++ PacketProcessor.py
@@ -2,18 +2,32 @@ import scapy.all as a
 from Config import KNOWN_PORTS
 
 
-def StructResult(pkt):
-    line= "\n--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
-    src_mac = pkt.src if hasattr(pkt, "src") else "N/A"
-    dst_mac = pkt.dst if hasattr(pkt, "dst") else "N/A"
+class AnsiColors:
+    RED = '\033[91m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
     
-    src_ip, dst_ip = "N/A", "N/A"
-    if pkt.haslayer(a.IP): 
-        src_ip = pkt[a.IP].src
-        dst_ip = pkt[a.IP].dst
-    elif pkt.haslayer(a.IPv6):  
-        src_ip = pkt[a.IPv6].src
-        dst_ip = pkt[a.IPv6].dst
-    
-    return "[Source MAC: " + src_mac + "] -> [Destination MAC: " + dst_mac + "] [Source IP: " + src_ip +"] -> [Destination IP: "+ dst_ip + "]" + line
+    ENDC = '\033[0m'
 
+def StructResult(pkt):
+    line= f"\n{AnsiColors.BLUE}--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------{AnsiColors.ENDC}"
+    
+    src_mac_val = pkt.src if hasattr(pkt, "src") else "N/A"
+    dst_mac_val = pkt.dst if hasattr(pkt, "dst") else "N/A"
+    
+    src_ip_val, dst_ip_val = "N/A", "N/A"
+    if pkt.haslayer(a.IP): 
+        src_ip_val = pkt[a.IP].src
+        dst_ip_val = pkt[a.IP].dst
+    elif pkt.haslayer(a.IPv6):  
+        src_ip_val = pkt[a.IPv6].src
+        dst_ip_val = pkt[a.IPv6].dst
+    src_mac_label = f"{AnsiColors.YELLOW}Source MAC{AnsiColors.ENDC}"
+    dst_mac_label = f"{AnsiColors.YELLOW}Destination MAC{AnsiColors.ENDC}"
+    src_ip_label = f"{AnsiColors.YELLOW}Source IP{AnsiColors.ENDC}"
+    dst_ip_label = f"{AnsiColors.YELLOW}Destination IP{AnsiColors.ENDC}"
+
+
+    return (f"[{src_mac_label}: {src_mac_val}] -> [{dst_mac_label}: {dst_mac_val}] "
+            f"[{src_ip_label}: {src_ip_val}] -> [{dst_ip_label}: {dst_ip_val}]" 
+            f"{line}")
